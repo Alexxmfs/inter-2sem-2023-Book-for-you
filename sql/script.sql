a33194dbea66de31966f68d67c41783c6086e146
Alex <88356153+Alexxmfs@users.noreply.github.com>
2023-10-20 10:15:06 -0300

    construindo listagem

--- sql/script.sql
+++ sql/script.sql
@@ -3,7 +3,7 @@ CREATE DATABASE BookDB;
 CREATE TABLE livro (
     idLivro INT NOT NULL AUTO_INCREMENT,
     titulo VARCHAR(50) NOT NULL,
-    `desc` VARCHAR(100) NOT NULL,
+    descricao VARCHAR(100) NOT NULL,
     autor VARCHAR(50) NOT NULL,
     editora VARCHAR(50) NOT NULL,
     categoria VARCHAR(20) NOT NULL,
